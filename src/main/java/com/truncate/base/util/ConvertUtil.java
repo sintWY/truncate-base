552e6f9545343eade7be34771068b317b5cfa2c8
lumia <524798219@qq.com>
2017-04-14 14:50:35 +0800

    升级依赖包common-lang的版本

--- src/main/java/com/truncate/base/util/ConvertUtil.java
+++ src/main/java/com/truncate/base/util/ConvertUtil.java
@@ -2,7 +2,7 @@ package com.truncate.base.util;
 
 import com.truncate.base.exception.CommonException;
 import com.truncate.base.exception.ErrorCode;
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
